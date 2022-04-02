a4c740d1b69b2bf77ee4fbb414cee5ba11147a06
AsumaPT <58395658+AsumaPT@users.noreply.github.com>
2022-04-02 14:38:06 +0100

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Portugal Overlay
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Orgulho tuga!
 // @author       Some anon
 // @match        https://hot-potato.reddit.com/embed*
@@ -14,7 +14,29 @@ if (window.top !== window.self) {
         (function () {
             const i = document.createElement("img");
             i.src = "https://raw.githubusercontent.com/AsumaPT/draconBot/main/canvas_pt.png";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px; opacity: 0.85";
+            i.style = "position: absolute;left: 0;top: 0px;image-rendering: pixelated;width: 1000px;height: 1000px; opacity: 0.75";
+            i.id = "mcss-overlay"
+            i.setAttribute("vis",1)
+            console.log(i);
+            return i;
+        })())
+
+            document.getElementsByTagName("body")[0].appendChild(
+        (function () {
+            const i = document.createElement("button");
+            i.style = "position: fixed;left: 10px; background: #fff; top: 50%;width: 50px;height: 50px;";
+            i.innerHTML = "T"
+            i.onclick = function() {
+                const img = document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.querySelector("#mcss-overlay")
+                if (img.getAttribute("vis") == "1") {
+                    img.setAttribute("vis",0)
+                    img.style.opacity  = 0
+                } else {
+
+                    img.setAttribute("vis",1)
+                    img.style.opacity  = 1
+                }
+            }
             console.log(i);
             return i;
         })())
